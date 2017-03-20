1ca40aa1a9db013c13668643e71e95654af070e6
kusumandaru <kusumandaru@users.noreply.github.com>
2017-03-20 12:07:30 +0700

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
 
         $schedule->command('email:send-timesheet-to-hr')
-            ->cron('0 3 * * 1 *');
+            ->cron('30 2 * * 1 *');
 
         $schedule->command('email:send-timesheet-to-finance')
             ->cron('0 0 1,15 * * *');
